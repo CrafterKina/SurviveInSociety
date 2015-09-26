c143d7758029128b113f855ccbb702322a9ae3f5
CrafterKina <kina_ugo@yahoo.co.jp>
2015-09-27 05:03:10 +0900

    added proxy system

---
+++ src/main/java/jp/crafterkina/SurviveInSociety/server/ServerProxy.java
@@ -0,0 +1,10 @@
+/*
+ * Copyright (c) 2015, CrafterKina
+ * All rights reserved.
+ */
+
+package jp.crafterkina.SurviveInSociety.server;
+
+import jp.crafterkina.KinaCore.misclib.base.fle.AbstractFMLStateEvent;
+
+public class ServerProxy extends AbstractFMLStateEvent{}
