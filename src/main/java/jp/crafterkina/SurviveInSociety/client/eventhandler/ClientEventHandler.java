abdf763d3b15128aa19251b846512c65d54b558c
CrafterKina <kina_ugo@yahoo.co.jp>
2015-09-27 23:22:20 +0900

    impl event system

---
+++ src/main/java/jp/crafterkina/SurviveInSociety/client/eventhandler/ClientEventHandler.java
@@ -0,0 +1,10 @@
+/*
+ * Copyright (c) 2015, CrafterKina
+ * All rights reserved.
+ */
+
+package jp.crafterkina.SurviveInSociety.client.eventhandler;
+
+public enum ClientEventHandler{
+    instance
+}
