db478bd6c98073d0bce2ece7fa2eadc908cc49e1
CrafterKina <kina_ugo@yahoo.co.jp>
2015-09-27 23:48:55 +0900

    add registering item

--- src/main/java/jp/crafterkina/SurviveInSociety/item/EnumItem.java
+++ src/main/java/jp/crafterkina/SurviveInSociety/item/EnumItem.java
@@ -5,17 +5,27 @@
 
 package jp.crafterkina.SurviveInSociety.item;
 
+import net.minecraft.item.Item;
+import net.minecraftforge.fml.common.registry.GameRegistry;
+
 public enum EnumItem{
     ;
 
     public static final EnumItem[] values = values();
+    private Item item;
+
+    EnumItem(Item item){
+        this.item = item;
+    }
 
     EnumItem(){
 
     }
 
     public static void registerItems(){
-
+        for(EnumItem enumItem : values){
+            GameRegistry.registerItem(enumItem.item, enumItem.name());
+        }
     }
 
     public static void registerModels(){
