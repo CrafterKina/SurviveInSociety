0c79e5fe1676819ec4ca3c9de0d2ca761355e84c
CrafterKina <kina_ugo@yahoo.co.jp>
2015-09-28 19:04:24 +0900

    fix calling model register

--- src/main/java/jp/crafterkina/SurviveInSociety/client/ClientProxy.java
+++ src/main/java/jp/crafterkina/SurviveInSociety/client/ClientProxy.java
@@ -8,11 +8,11 @@ package jp.crafterkina.SurviveInSociety.client;
 import jp.crafterkina.KinaCore.misclib.base.fle.AbstractFMLStateEvent;
 import jp.crafterkina.SurviveInSociety.block.EnumBlock;
 import jp.crafterkina.SurviveInSociety.item.EnumItem;
-import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
 public class ClientProxy extends AbstractFMLStateEvent{
     @Override
-    public void init(FMLInitializationEvent event){
+    public void preInit(FMLPreInitializationEvent event){
         EnumBlock.registerModels();
         EnumItem.registerModels();
     }
