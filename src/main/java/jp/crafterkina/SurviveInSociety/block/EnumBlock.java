dda9d08cffbfc614386b7a6b1a4a178de7ecf494
CrafterKina <kina_ugo@yahoo.co.jp>
2015-09-28 18:27:32 +0900

    add common block register method

--- src/main/java/jp/crafterkina/SurviveInSociety/block/EnumBlock.java
+++ src/main/java/jp/crafterkina/SurviveInSociety/block/EnumBlock.java
@@ -5,8 +5,16 @@
 
 package jp.crafterkina.SurviveInSociety.block;
 
+import net.minecraft.block.Block;
+import net.minecraft.item.Item;
+import net.minecraftforge.fml.common.registry.GameData;
+import net.minecraftforge.fml.common.registry.GameRegistry;
+import net.minecraftforge.fml.relauncher.ReflectionHelper;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 public enum EnumBlock{
     ;
@@ -15,6 +23,23 @@ public enum EnumBlock{
 
     EnumBlock(){
 
+    }
+
+    @SuppressWarnings("unchecked")
+    public static void registerBlock(Block block, Item item, String name){
+        Method main = ReflectionHelper.findMethod(GameData.class, null, new String[]{"getMain"});
+        Method register = ReflectionHelper.findMethod(GameData.class, null, new String[]{"registerBlock"}, Block.class, String.class);
+        try{
+            register.invoke(main, block, name);
+            if(item != null){
+                GameRegistry.registerItem(item, name);
+                GameData.getBlockItemMap().put(block, item);
+            }
+        }catch(IllegalAccessException e){
+            throw new AssertionError(e);
+        }catch(InvocationTargetException e){
+            throw new AssertionError(e);
+        }
     }
 
     public static void registerBlocks(){
