a5a89f4c0d2b71d768ab29362437edb775666a9b
CrafterKina <kina_ugo@yahoo.co.jp>
2015-09-30 00:34:05 +0900

    add basic AIs

--- src/main/java/jp/crafterkina/SurviveInSociety/entity/entities/EntityReceptionist.java
+++ src/main/java/jp/crafterkina/SurviveInSociety/entity/entities/EntityReceptionist.java
@@ -7,10 +7,15 @@ package jp.crafterkina.SurviveInSociety.entity.entities;
 
 import net.minecraft.entity.EntityLiving;
 import net.minecraft.entity.INpc;
+import net.minecraft.entity.ai.EntityAISwimming;
+import net.minecraft.pathfinding.PathNavigateGround;
 import net.minecraft.world.World;
 
 public class EntityReceptionist extends EntityLiving implements INpc{
     public EntityReceptionist(World worldIn){
         super(worldIn);
+        this.setSize(0.6F, 1.8F);
+        ((PathNavigateGround) this.getNavigator()).setAvoidsWater(true);
+        this.tasks.addTask(0, new EntityAISwimming(this));
     }
 }
