86e14cdacd994b8de7272b40d13bf3deb0bfea0f
CrafterKina <kina_ugo@yahoo.co.jp>
2015-10-03 07:01:43 +0900

    add register method

--- src/main/java/jp/crafterkina/SurviveInSociety/world/village/EnumVillageStructure.java
+++ src/main/java/jp/crafterkina/SurviveInSociety/world/village/EnumVillageStructure.java
@@ -5,4 +5,20 @@
 
 package jp.crafterkina.SurviveInSociety.world.village;
 
-public enum EnumVillageStructure{}
+import net.minecraftforge.fml.common.registry.VillagerRegistry;
+
+public enum EnumVillageStructure{
+    ;
+
+    private final VillagerRegistry.IVillageCreationHandler handler;
+
+    EnumVillageStructure(VillagerRegistry.IVillageCreationHandler handler){
+        this.handler = handler;
+    }
+
+    public static void register(){
+        for(EnumVillageStructure value : values()){
+            VillagerRegistry.instance().registerVillageCreationHandler(value.handler);
+        }
+    }
+}
