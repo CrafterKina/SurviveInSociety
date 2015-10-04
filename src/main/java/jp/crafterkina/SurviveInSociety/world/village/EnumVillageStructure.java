d5a44daceafd9bd6115b7a6eafb50cd288f3fb2e
CrafterKina <kina_ugo@yahoo.co.jp>
2015-10-04 10:02:22 +0900

    add component registration

--- src/main/java/jp/crafterkina/SurviveInSociety/world/village/EnumVillageStructure.java
+++ src/main/java/jp/crafterkina/SurviveInSociety/world/village/EnumVillageStructure.java
@@ -5,6 +5,8 @@
 
 package jp.crafterkina.SurviveInSociety.world.village;
 
+import net.minecraft.world.gen.structure.MapGenStructureIO;
+import net.minecraft.world.gen.structure.StructureVillagePieces;
 import net.minecraftforge.fml.common.registry.VillagerRegistry;
 
 public enum EnumVillageStructure{
@@ -12,13 +14,16 @@ public enum EnumVillageStructure{
     public static final EnumVillageStructure[] values = values();
 
     private final VillagerRegistry.IVillageCreationHandler handler;
+    private final Class<? extends StructureVillagePieces.Village> component;
 
-    EnumVillageStructure(VillagerRegistry.IVillageCreationHandler handler){
+    EnumVillageStructure(VillagerRegistry.IVillageCreationHandler handler, Class<? extends StructureVillagePieces.Village> component){
         this.handler = handler;
+        this.component = component;
     }
 
     public static void register(){
         for(EnumVillageStructure value : values){
+            MapGenStructureIO.registerStructureComponent(value.component, value.name());
             VillagerRegistry.instance().registerVillageCreationHandler(value.handler);
         }
     }
