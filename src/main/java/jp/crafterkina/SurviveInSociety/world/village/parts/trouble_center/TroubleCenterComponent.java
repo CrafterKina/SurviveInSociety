f6e657d6e2ae9802e5edcfee37489021615cb72c
CrafterKina <kina_ugo@yahoo.co.jp>
2015-10-04 13:54:13 +0900

    provisional impl

--- src/main/java/jp/crafterkina/SurviveInSociety/world/village/parts/trouble_center/TroubleCenterComponent.java
+++ src/main/java/jp/crafterkina/SurviveInSociety/world/village/parts/trouble_center/TroubleCenterComponent.java
@@ -5,13 +5,19 @@
 
 package jp.crafterkina.SurviveInSociety.world.village.parts.trouble_center;
 
+import net.minecraft.util.EnumFacing;
 import net.minecraft.world.World;
 import net.minecraft.world.gen.structure.StructureBoundingBox;
 import net.minecraft.world.gen.structure.StructureVillagePieces;
 
+import java.util.List;
 import java.util.Random;
 
 public class TroubleCenterComponent extends StructureVillagePieces.Village{
+    protected static TroubleCenterComponent build(StructureVillagePieces.PieceWeight villagePiece, StructureVillagePieces.Start startPiece, List pieces, Random random, int p1, int p2, int p3, EnumFacing facing, int p5){
+        return null;
+    }
+
     @Override
     public boolean addComponentParts(World worldIn, Random randomIn, StructureBoundingBox structureBoundingBoxIn){
         return false;
--- src/main/java/jp/crafterkina/SurviveInSociety/world/village/parts/trouble_center/TroubleCenterCreationHandler.java
+++ src/main/java/jp/crafterkina/SurviveInSociety/world/village/parts/trouble_center/TroubleCenterCreationHandler.java
@@ -6,25 +6,26 @@
 package jp.crafterkina.SurviveInSociety.world.village.parts.trouble_center;
 
 import net.minecraft.util.EnumFacing;
-import net.minecraft.world.gen.structure.StructureVillagePieces;
-import net.minecraftforge.fml.common.registry.VillagerRegistry;
+import net.minecraft.world.gen.structure.StructureVillagePieces.PieceWeight;
+import net.minecraft.world.gen.structure.StructureVillagePieces.Start;
+import net.minecraftforge.fml.common.registry.VillagerRegistry.IVillageCreationHandler;
 
 import java.util.List;
 import java.util.Random;
 
-public class TroubleCenterCreationHandler implements VillagerRegistry.IVillageCreationHandler{
+public class TroubleCenterCreationHandler implements IVillageCreationHandler{
     @Override
-    public StructureVillagePieces.PieceWeight getVillagePieceWeight(Random random, int i){
-        return null;
+    public PieceWeight getVillagePieceWeight(Random random, int i){
+        return new PieceWeight(TroubleCenterComponent.class, 20, 1);
     }
 
     @Override
     public Class<?> getComponentClass(){
-        return null;
+        return TroubleCenterComponent.class;
     }
 
     @Override
-    public Object buildComponent(StructureVillagePieces.PieceWeight villagePiece, StructureVillagePieces.Start startPiece, List pieces, Random random, int p1, int p2, int p3, EnumFacing facing, int p5){
-        return null;
+    public Object buildComponent(PieceWeight villagePiece, Start startPiece, List pieces, Random random, int p1, int p2, int p3, EnumFacing facing, int p5){
+        return TroubleCenterComponent.build(villagePiece, startPiece, pieces, random, p1, p2, p3, facing, p5);
     }
 }
