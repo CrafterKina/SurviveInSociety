c1ff3a122d1cb0f87b3b9328f2ede11b162067e5
CrafterKina <kina_ugo@yahoo.co.jp>
2015-10-17 00:18:35 +0900

    override general methods

--- src/main/java/jp/crafterkina/SurviveInSociety/block/blocks/BlockBulletinBoard.java
+++ src/main/java/jp/crafterkina/SurviveInSociety/block/blocks/BlockBulletinBoard.java
@@ -5,11 +5,32 @@
 
 package jp.crafterkina.SurviveInSociety.block.blocks;
 
-import net.minecraft.block.Block;
+import net.minecraft.block.BlockContainer;
 import net.minecraft.block.material.Material;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.EnumWorldBlockLayer;
+import net.minecraft.world.World;
 
-public class BlockBulletinBoard extends Block{
+public class BlockBulletinBoard extends BlockContainer{
     public BlockBulletinBoard(){
         super(Material.rock);
+    }
+
+    @Override
+    public TileEntity createNewTileEntity(World worldIn, int meta){
+        return null;
+    }
+
+    /**
+     * The type of render function that is called for this block
+     */
+    @Override
+    public int getRenderType(){
+        return 3;
+    }
+
+    @Override
+    public EnumWorldBlockLayer getBlockLayer(){
+        return EnumWorldBlockLayer.CUTOUT_MIPPED;
     }
 }
