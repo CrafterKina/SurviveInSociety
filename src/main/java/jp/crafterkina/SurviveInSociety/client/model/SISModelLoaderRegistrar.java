32d1d0e022f694fd4f80b54ac108e1f6ebdbeda8
CrafterKina <kina_ugo@yahoo.co.jp>
2015-10-21 21:40:41 +0900

    change to class

--- src/main/java/jp/crafterkina/SurviveInSociety/client/model/SISModelLoaderRegistrar.java
+++ src/main/java/jp/crafterkina/SurviveInSociety/client/model/SISModelLoaderRegistrar.java
@@ -5,51 +5,14 @@
 
 package jp.crafterkina.SurviveInSociety.client.model;
 
-import com.google.common.collect.Maps;
-import net.minecraft.client.resources.IResourceManager;
-import net.minecraft.util.ResourceLocation;
-import net.minecraftforge.client.model.ICustomModelLoader;
-import net.minecraftforge.client.model.IModel;
-import net.minecraftforge.client.model.ModelLoaderRegistry;
-
-import java.io.IOException;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
-import java.util.Map;
 
-public enum SISModelLoaderRegistrar implements ICustomModelLoader{
-    Common{
-        private Map<String,IModel> modelMap = Maps.newHashMap();
-
-        @Override
-        public void onResourceManagerReload(IResourceManager resourceManager){
-
-        }
-
-        @Override
-        public boolean accepts(ResourceLocation modelLocation){
-            return modelMap.containsKey(modelLocation.toString());
-        }
-
-        @Override
-        public IModel loadModel(ResourceLocation modelLocation) throws IOException{
-            return modelMap.get(modelLocation.toString());
-        }
-
-        public void registerModel(ResourceLocation location, IModel model){
-            String path = location.getResourcePath();
-            path = (path.startsWith("builtin/") || path.startsWith("models/") ? "" : "models/") + path;
-            location = new ResourceLocation(location.getResourceDomain(), path);
-            modelMap.put(location.toString(), model);
-        }
-    };
-
+public class SISModelLoaderRegistrar{
     public static void register(){
-        for(SISModelLoaderRegistrar loader : values()){
-            ModelLoaderRegistry.registerLoader(loader);
-        }
+
     }
 
     @Retention(RetentionPolicy.RUNTIME)
