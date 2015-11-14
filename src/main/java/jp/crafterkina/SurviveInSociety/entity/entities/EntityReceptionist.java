7867a4afdd9359aa34663eddccd863b4752aae05
CrafterKina <kina_ugo@yahoo.co.jp>
2015-11-15 00:10:48 +0900

    add watch closest ai

--- src/main/java/jp/crafterkina/SurviveInSociety/entity/entities/EntityReceptionist.java
+++ src/main/java/jp/crafterkina/SurviveInSociety/entity/entities/EntityReceptionist.java
@@ -8,6 +8,8 @@ package jp.crafterkina.SurviveInSociety.entity.entities;
 import net.minecraft.entity.EntityLiving;
 import net.minecraft.entity.INpc;
 import net.minecraft.entity.ai.EntityAISwimming;
+import net.minecraft.entity.ai.EntityAIWatchClosest;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.pathfinding.PathNavigateGround;
 import net.minecraft.world.World;
 
@@ -17,5 +19,6 @@ public class EntityReceptionist extends EntityLiving implements INpc{
         this.setSize(0.6F, 1.8F);
         ((PathNavigateGround) this.getNavigator()).setAvoidsWater(true);
         this.tasks.addTask(0, new EntityAISwimming(this));
+        this.tasks.addTask(1, new EntityAIWatchClosest(this, EntityPlayer.class, 10, 1));
     }
 }
